04d7df24dfdb3dbf5802c7b5cc5dfe78815da212
Vatsal Agrawal <123290722+Vatsal-A@users.noreply.github.com>
2025-12-05 18:25:37 -0500

    Update THREE.js imports to use CDN links

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.162.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.162.0/examples/jsm/controls/OrbitControls.js';
+
 
 // ------ Basic Setup ------
 const canvas = document.querySelector('canvas.webgl');
@@ -257,3 +258,4 @@ window.addEventListener('resize', () => {
   camera.updateProjectionMatrix();
   renderer.setSize(w, h);
 });
+
